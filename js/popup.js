7a697a2eb70791006978aa124fafd88dbe196a4b
Kevin Cho <chokevinj@gmail.com>
2016-12-08 18:35:03 -0500

    Clean up code

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   document.getElementById('search-btn').addEventListener('click', function() {
     sendSearch(document.getElementById('search-tb').value);
   }, false);
+
   document.getElementById('search-tb').addEventListener('keydown', function(event) {
     if (event.which == 13) {
-      sendSearch(document.getElementById('search-tb').value);
+      document.getElementById('search-btn').click();
     }
   }, false);
+
 }, false);
