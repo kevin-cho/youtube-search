40000e12646b348ac21d10484398275965620200
Kevin Cho <chokevinj@gmail.com>
2017-06-17 11:21:16 -0400

    Replace keydown handler with form submit

--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.getElementById('search-btn').addEventListener('click', function() {
     sendSearch(document.getElementById('search-tb').value);
-  }, false);
+  });
 
-  document.getElementById('search-tb').addEventListener('keydown', function(event) {
-    if (event.which == 13) {
-      document.getElementById('search-btn').click();
-    }
-  }, false);
-
-}, false);
+});
